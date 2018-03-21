f0b3f2b7e764f61041f33a79d69e7f4a93852842
MingmingLiuml4077 <35637307+MingmingLiuml4077@users.noreply.github.com>
2018-03-21 11:47:21 -0400

    Add files via upload

--- lib/test.R
+++ lib/test.R
@@ -2,25 +2,84 @@
 ### Fit the classification model with testing data ###
 ######################################################
 
-### Author: Yuting Ma
-### Project 3
-### ADS Spring 2016
+### Author: Mingming Liu
+### Project 3 group3
+### ADS Spring 2018
 
-test <- function(fit_train, dat_test){
-  
-  ### Fit the classfication model with testing data
-  
-  ### Input: 
-  ###  - the fitted classification model using training data
-  ###  -  processed features from testing images 
-  ### Output: training model specification
-  
-  ### load libraries
-  library("gbm")
-  
-  pred <- predict(fit_train$fit, newdata=dat_test, 
-                  n.trees=fit_train$iter, type="response")
-  
-  return(as.numeric(pred> 0.5))
+### Fit the classfication model with testing data
+
+### Input: 
+###  - the fitted classification model using training data
+###  -  processed features from testing images 
+### Output: training model specification
+
+####GBM by Zhongxing Xue
+gbm_test <- function(gbm_fit, TestData)
+{
+  Test.pred <- predict(object = gbm_fit, newdata = TestData, n.trees = gbm.perf(gbm_fit, plot.it = FALSE), type = "response")
+  Test.pred <- matrix(Test.pred, ncol =3)
+  Test.pred <- apply(Test.pred, 1, which.max)
+  return(Test.pred)
 }
 
+
+
+####randomForest by Mingming Liu
+rf_test <- function(fit_train,test_data){
+  pred <- predict.boosting(adaall$fit,newdata=test_xall)
+  return(pred)
+}
+
+
+####svm by Yuhan Cha
+svm_test<- function(fit_model, dat_test){
+  
+  pred <- predict(fit_model, 
+                  testdata)
+return(pred)
+}
+
+####tree model by Yuhan Cha
+tree_test<- function(fit_model, dat_test){
+  
+  pred <- predict(fit_model, 
+                  testdata, 
+                  type = "class")
+  return(pred)
+}
+
+####Adaboosting by Mingming Liu
+adaboost_test<- function(fit_model, dat_test){
+  predict <- predict.boosting(adaall$fit,newdata=test_xall)
+  adapredict <- as.numeric(predict$class)
+  return(adapredict)
+}
+
+####CNN by Keran Li
+
+nn_test<- function(fit_model, testdata){
+  
+  pred<- round(compute(fit_model,testdata),0)
+  return(pred)
+}
+
+
+####logistic by Keran Li
+logistic_test<- function(fit_model,testdata){
+  
+  pred <- predict(fit_model, 
+                  newdata=testdata, 
+                  type = "class")
+  return(pred)
+}
+
+
+####Xgboost by Junkai Zhang
+xgboost_test<- function(fit_model, testdata){
+  # fit_model should be xgb$fit and testdata has to be a matrix
+  xgb.test.data <- xgb.DMatrix(data = testdata)
+  pred <- predict(fit_model,newdata = xgb.test.data)
+  return(pred)
+}
+#pred <- xgboost_test(xgb$fit,testdata) # predicted output of the xgboost is 0, 1 and 2.
+#sum(pred==test.class[,2] - 1)/nrow(test.class)
